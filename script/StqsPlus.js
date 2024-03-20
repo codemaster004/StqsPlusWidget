ae6a36fb1cb0df9f3e3796efdf1a016d9c2ec419
Filip Dabkowski <filip.dabkowski@gmail.com>
2024-03-20 11:31:06 +0100

    rhs margin left and repainr error message visibility bug

--- script/StqsPlus.js
+++ script/StqsPlus.js
@@ -1,3 +1,8 @@
 window.onload=function() {
     document.body.classList.add('dark');
+
+    let errorDiv = document.getElementById("error");
+    if (errorDiv.innerHTML === "") {
+        errorDiv.style.display = "none";
+    }
 }
