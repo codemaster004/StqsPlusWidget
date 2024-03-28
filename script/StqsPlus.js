560dec8cf1dc4c5f88044143a5e67aa6a6b3e8ac
Filip Dabkowski <filip.dabkowski@gmail.com>
2024-03-28 12:57:58 +0100

    gradient done

--- script/StqsPlus.js
+++ script/StqsPlus.js
@@ -1,3 +1,11 @@
+function getColors(color1, color2, precent) {
+    let colors = [];
+    for (let i = 0; i < color1.length; i++) {
+        colors.push(color1[i] + (color2[i] - color1[i]) * precent);
+    }
+    return colors;
+}
+
 window.onload=function() {
     document.body.classList.add('dark');
 
@@ -5,4 +13,28 @@ window.onload=function() {
     if (errorDiv.innerHTML === "") {
         errorDiv.style.display = "none";
     }
+
+    let gradientColors = [[237,104,96], [155,60,185], [78,166,96]]
+    document.querySelectorAll("td").forEach((element) => {
+        if (element.classList.length > 0 && element.classList[0].includes("score")) {
+            let aScore = element.querySelector("a")
+            if (aScore === null) {
+                return;
+            }
+            let score = aScore.innerHTML;
+            let precent = parseFloat(score.trim()) / 100;
+            console.log(precent);
+            if (precent >= 0.5) {
+                precent -= 0.5;
+                precent *= 2;
+                let colors = getColors(gradientColors[1], gradientColors[2], precent);
+                element.style.backgroundColor = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+            } else {
+                precent *= 2;
+                let colors = getColors(gradientColors[0], gradientColors[1], precent);
+                element.style.backgroundColor = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+            }
+
+        }
+    })
 }
